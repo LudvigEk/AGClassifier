9c23316faebee293706f861ebefa98e1d94c5dbb
Ludvig <ludvig.ekdahl@med.lu.se>
2023-01-16 13:13:21 +0100

    "WIP event_loop"

--- AGClassifier_event_manager.py
+++ AGClassifier_event_manager.py
@@ -4,7 +4,9 @@
 import PySimpleGUI as sg
 
 from AGClassifier_utilities import createInvalidSelectWindow, createInvalidCustomWindow, \
-    createNoSamplePDF, createCompleteWindow, get_page, checkIfDiscarded, checkIfInIndexFiles
+    createNoSamplePDF, createCompleteWindow, get_page, checkIfDiscarded, checkIfInIndexFiles, \
+    createPDFWindow, start_gating
+
 
 def check_event_categories(event_list, event_descriptor_dict):
     # Check that only one event of each category is present
@@ -12,9 +14,12 @@ def check_event_categories(event_list, event_descriptor_dict):
     # Custom_ events can be selected multiple times
     event_categories = []
     for event in event_list:
-        event_category = event_descriptor_dict[event].split("_")[0]
-        if event_category is not "CUSTOM_":
-            event_categories.append(event_category)
+        if "_" in event:
+            event_category = event_descriptor_dict[event].split("_")[0]
+            if event_category is not "CUSTOM_":
+                event_categories.append(event_category)
+        else:
+            event_categories.append(event)
     event_categories = set(event_categories)
     if len(event_categories) < len(event_list):
         createInvalidSelectWindow()
@@ -22,10 +27,8 @@ def check_event_categories(event_list, event_descriptor_dict):
     else:
         return True
 
+
 def limit_event_handler(event_list, output_folder, event_descriptor_dict):
-
-
-
     # Check if event exists in the button_descriptor_dict
     # If not, raise
     for event in event_list:
@@ -40,16 +43,37 @@ def limit_event_handler(event_list, output_folder, event_descriptor_dict):
         else:
             raise_str = "Event " + str(event) + " not found in event_descriptor_dict and is not a custom event"
             raise ValueError(raise_str)
-
-
     return
 
-def context_event_handler(event, values, input_folder, output_folder):
+
+def context_event_handler(event_list, input_folder, output_folder):
     # TODO
     # Handle static, contextual events
     # These are events that are not defined in the event_descriptor_dict
-
+    for event in event_list:
+        print(event)
     return
+
+
+def event_handler(event_list, input_folder, output_folder, event_descriptor_dict):
+    # TODO
+    # Handle events
+    # At this point the events are valid (check_event_categories has been run)
+    # Thus, we sort the event_list into limit and context events
+    # We call the context event handler first, then the limit event handler
+    # 'START', 'Done, next image', 'Previous image' and 'Exit' are handled in the event loop
+    context_events = []
+    limit_events = []
+    for event in event_list:
+        if event.split("_")[0] in ["DISCARD", "Set this pop NA", "Open pdf", ]:
+            context_events.append(event)
+        else:
+            limit_events.append(event)
+    context_event_handler(context_events, input_folder, output_folder)
+    limit_event_handler(limit_events, output_folder, event_descriptor_dict)
+    return
+
+
 def next_sample():
     # TODO
     # Get the next sample
@@ -58,26 +82,72 @@ def next_sample():
     # If the next sample is not discarded or analysed, display it and return True
     return True
 
+
+def previous_sample():
+    # TODO
+    # Get the previous sample
+    # If there is no previous sample, do nothing
+    # If the previous sample is discarded, skip it and go to the previous one
+    # Until a non-discarded sample is found or there are no more samples, in which case do nothing
+    return True
+
+
+def is_context_event(event):
+    if event in ["START", "Done, next image", "Previous image", "Exit", "Open pdf", "Set this pop NA", "Discard"]:
+        return True
+    else:
+        return False
+
+def initialise_parameters():
+    # TODO
+    # Initialise parameters
+    return
+
 def event_loop(window, input_folder, output_folder, event_descriptor_dict):
+    #
+    #
+    #
+    image_index = 0
+    file_list = []
+    initialise_parameters()
+
     # Initialise the event list with no elements
     event_list = []
     while True:
         event, values = window.read()
-        if event == "Exit" or event == sg.WIN_CLOSED:
-            break
-        elif event == "DONE":
-            # First check that the event list is valid
-            # This spawns a invalid selection window if not
-            if check_event_categories(event_list, event_descriptor_dict):
-                # If so, run the event handler
-                limit_event_handler(event_list, output_folder, event_descriptor_dict)
-                # then clear the event list and go to the next sample
-                event_list = []
-                if not next_sample():
-                    break
+        # First check for context events (start, exit, previous, discard, set to na, open pdf)
+        if is_context_event(event):
+            # The open pdf event is a special case, it does not clear the event list
+            if event == 'Open pdf':
+                if createPDFWindow():  # Returns True if pdf exists and is opened, False otherwise
+                    continue
             else:
-                # Otherwise, clear the event list and keep going on the same sample
                 event_list = []
-        else:
-            event_list.append(event)
+                if event in ["set to na", "discard"]:
+                    # These events should trigger a next sample call
+                    next_sample()
+                elif event in ["previous"]:
+                    previous_sample()
+                else:
+                    # Trigger start gating at sample X or from the beginning
+                    start_gating(window_ref=window, )  # TODO
+                    continue
+
+        if not context_event_handler(event, event_list):
+            # If the event recieves
+            if event == "DONE, next image":
+                # First check that the event list is valid
+                # This spawns an invalid selection window if not
+                if check_event_categories(event_list, event_descriptor_dict):
+                    # If so, run the event handler
+                    limit_event_handler(event_list, output_folder, event_descriptor_dict)
+                    # then clear the event list and go to the next sample
+                    event_list = []
+                    if not next_sample():
+                        break
+                else:
+                    # Otherwise, clear the event list and keep going on the same sample
+                    event_list = []
+            else:
+                event_list.append(event)
     return
--- AGClassifier_utilities.py
+++ AGClassifier_utilities.py
@@ -2,39 +2,178 @@
 # -*- coding: utf-8 -*-
 
 import PySimpleGUI as sg
+import sys, os, fitz
+
 
 # Wrong selection window
 def createInvalidSelectWindow():
+    reportStr = "Invalid selection, more than one in each category (xlim/ylim) is not allowed.\n"
+    sys.stderr.write(reportStr)
     invalid_selection_layout = [
         [
             sg.Text(
                 'Invalid selection, too many X- or Y- limits\nYou may only select up to one X-limit and up to one '
-                'Y-limit.\nIt is allowed to select no limit at all for an image, as well as just one X- or Y- limit'),
+                'Y-limit.\nIt is allowed to select no limit at all for an image, as well as just one X- or Y- '
+                'limit\nand an unlimited amount of custom limits.'),
             sg.Button('OK', size=(8, 4))
         ]
     ]
-    return sg.Window("Invalid Selection", invalid_selection_layout)
+    invalid_select_window = sg.Window("Invalid Selection", invalid_selection_layout)
+    debug_event, debug_values = invalid_select_window.read()
+    if debug_event == "OK" or debug_event == sg.WIN_CLOSED:
+        invalid_select_window.close()
+    return
+
+
+def start_gating(window_ref, image_index, file_list, page_no = 0):
+    """
+    -
+    :return:
+    """
+    if not isinstance(page_no, int):
+        warnMSG = "Page number is not an integer, defaulting to 0"
+        sys.stderr.write(warnMSG)
+        # TODO - create warning window
+        page_no = 0
+        pass
+
+    # image_index = 0 #image index already initialized and might have been set
+    while checkIfDiscarded(image_index, file_list):
+        image_index += 1
+
+    filename = os.path.join(
+
+        window_ref["-FOLDER-"], file_list[image_index]
+
+    )
+
+    cleaned_name = cleaned_name = os.path.basename(filename).replace(".pdf",
+                                                                     "")  # filename.split("/")[-1].replace(".pdf", "")
+    window_ref["-TOUT-"].update(cleaned_name)
+    countStr = str(image_index) + "/" + str(len(file_list))
+    window_ref["-COUNT-"].update(countStr)
+    # Open image X from pdf
+    try:
+        doc = fitz.open(filename)
+    except FileNotFoundError:
+        raise
+    page_count = len(doc)
+    dlist_tab = [None] * page_count
+    cur_page = page_no
+
+    data = get_page(cur_page, dlist_tab, doc)  # show page 1 for start
+
+    window_ref["-IMAGE-"].update(data=data, size=(320, 280))
+
+    # Check if in any index:
+    sampleInIndexFiles = checkIfInIndexFiles(image_index, file_list)
+    window_ref["-INDEX-"].update(sampleInIndexFiles)
+
+
+def createPDFWindow(fname, ID):
+    """
+    Creates a window with a PDF of all images belonging to the same sample.
+    :param fname: filename containing the PDF
+    :param ID: sample ID
+    :return:
+    """
+    #
+    try:
+        doc = fitz.open(fname)
+    except FileNotFoundError:
+        # Maybe print error?
+        # Maybe done in the calling function?
+        createNoSamplePDF()
+        return False
+    page_count = len(doc)
+    dlist_tab = [None] * page_count
+    cur_page = 0
+    old_page = 0
+
+    data = get_page(cur_page, dlist_tab, doc)  # show page 1 for start
+    image_elem = sg.Image(data=data)
+    goto = sg.InputText(str(cur_page + 1), size=(5, 1))
+
+    layout = [
+        [
+            sg.Button('Prev'),
+            sg.Button('Next'),
+            sg.Text('Page:'),
+            goto,
+        ],
+        [image_elem],
+    ]
+    my_keys = ("Next", "Next:34", "Prev", "Prior:33", "MouseWheel:Down", "MouseWheel:Up")
+
+    window = sg.Window(ID, layout,
+                       return_keyboard_events=True, use_default_focus=False)
+
+    while True:
+        event, values = window.read(timeout=100)
+        force_page = False
+        if event == sg.WIN_CLOSED:
+            break
+        if event in ("Escape:27",):  # this spares me a 'Quit' button!
+            break
+        if event[0] == chr(13):  # surprise: this is 'Enter'!
+            try:
+                cur_page = int(values[0]) - 1  # check if valid
+                while cur_page < 0:
+                    cur_page += page_count
+            except IndexError:
+                cur_page = 0  # this guy's trying to fool me
+            goto.update(str(cur_page + 1))
+        elif event in ("Next", "Next:34", "MouseWheel:Down"):
+            cur_page += 1
+        elif event in ("Prev", "Prior:33", "MouseWheel:Up"):
+            cur_page -= 1
+
+        if cur_page >= page_count:  # wrap around
+            cur_page = 0
+        while cur_page < 0:  # we show conventional page numbers
+            cur_page += page_count
+        if event in my_keys or not values[0]:
+            goto.update(str(cur_page + 1))
+
+        if cur_page != old_page:
+            force_page = True
+        # Update
+        if force_page:
+            data = get_page(cur_page, dlist_tab, doc)
+            image_elem.update(data=data)
+            old_page = cur_page
+    return True
 
 
 def createInvalidCustomWindow(label, value):
     reportStr = "Invalid custom " + str(label) + " limit, exepected integer (whole number), found: " + str(value)
+    sys.stderr.write(reportStr)
     invalid_custom_limit_layout = [
         [
             sg.Text(reportStr),
             sg.Button('OK', size=(8, 4))
         ]
     ]
-    return sg.Window("Invalid custom limit", invalid_custom_limit_layout)
+    invalidCustomWindow = sg.Window("Invalid custom limit", invalid_custom_limit_layout)
+    debug_event, debug_values = invalidCustomWindow.read()
+    if debug_event == "OK" or debug_event == sg.WIN_CLOSED:
+        invalidCustomWindow.close()
+    return
 
 
 def createNoSamplePDF():
+    # TODO - specify expected filename/path of the missing pdf ?
     missing_file = [
         [
             sg.Text('No .pdf file found for the sample.'),
             sg.Button('OK', size=(8, 4))
         ]
     ]
-    return sg.Window("Missing PDF", missing_file)
+    NoPDF_window = sg.Window("Missing PDF", missing_file)
+    debug_event, debug_values = NoPDF_window.read()
+    if debug_event == "OK" or debug_event == sg.WIN_CLOSED:
+        NoPDF_window.close()
+        return
 
 
 def createCompleteWindow():
@@ -46,11 +185,12 @@ def createCompleteWindow():
     ]
     return sg.Window("Complete", layout)
 
+
 def get_page(pno, dlist_tab, doc):
-    reportStr="doclength: "+str(len(doc))+" pageno: "+str(pno)
+    reportStr = "doclength: " + str(len(doc)) + " pageno: " + str(pno)
     print(reportStr)
     if len(doc) <= pno:
-        pno = len(doc)-1
+        pno = len(doc) - 1
     dlist = dlist_tab[pno]
     if not dlist:  # create if not yet there
         dlist_tab[pno] = doc[pno].getDisplayList()
@@ -112,6 +252,7 @@ def checkIfInIndexFiles(image_index, file_list):
     for entry in files_w_sample:
         outStr = entry + " " + outStr
     return outStr
+
 
 def createPDFWindow(fname, ID):
     try:
